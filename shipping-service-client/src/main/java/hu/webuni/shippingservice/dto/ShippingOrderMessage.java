dbf7be8af483d026ef31fa1fe0ec0982a8c07531
patonaiszabolcs <patonai.szabolcs@gmail.com>
2022-12-12 23:49:46 +0100

    TaskIII- Shipping service JMS communication

--- shipping-service-client/src/main/java/hu/webuni/shippingservice/dto/ShippingOrderMessage.java
+++ shipping-service-client/src/main/java/hu/webuni/shippingservice/dto/ShippingOrderMessage.java
@@ -1,5 +1,6 @@
 package hu.webuni.shippingservice.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,12 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Data
-public class OrderItemDTO {
+public class ShippingOrderMessage {
 
-    private Long id;
-    private  String name;
-    private Long price;
-    private String category;
-    private Long quantity;
+    private String orderStatus;
+    private String externalId;
+
 
 }
